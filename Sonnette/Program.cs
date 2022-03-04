1aa1873bccbab5fec2b9af73afa0c2f362cd488a
martin <martinrchrd96@gmail.com>
2022-03-04 11:21:22 +0100

    init project

---
+++ Sonnette/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Device.Gpio;
+
+namespace Sonnette.Raspi;
+
+public class Program {
+    public static void Main(string[] args) {
+        // See https://aka.ms/new-console-template for more information
+        Console.WriteLine("Hello, World!");
+
+        GpioController controller = new GpioController(PinNumberingScheme.Board);
+        controller.OpenPin(10, PinMode.Output);
+
+        while (true)
+        {
+            controller.Write(10, PinValue.Low);
+            Thread.Sleep(1000);
+            controller.Write(10, PinValue.High);
+            Thread.Sleep(1000);
+        }
+    }
+}
